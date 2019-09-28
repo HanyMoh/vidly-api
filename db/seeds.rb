fe4fc563f38eff24c2520d4b9d6c6cabaa3e3810
HanyMoh <hanymoh@msn.com>
2019-09-28 18:16:37 +0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Genre.count == 0
+  Genre.create! [
+    { name: 'Comedy' },
+    { name: 'Action' },
+    { name: 'Romance' },
+    { name: 'Thriller' }
+  ]
+  p "#{DateTime.now} Created Genres, (#{Genre.count} items)."
+end
+
+if Movie.count == 0
+  comedy = Genre.find_by(name: 'Comedy')
+  Movie.create! [
+    { title: "Airplane", number_in_stock: 5, daily_rental_rate: 2, genre_id: comedy.id },
+    { title: "The Hangover", number_in_stock: 10, daily_rental_rate: 2, genre_id: comedy.id },
+    { title: "Wedding Crashers", number_in_stock: 15, daily_rental_rate: 2, genre_id: comedy.id }
+  ]
+
+  action = Genre.find_by(name: 'Action')
+  Movie.create! [
+    { title: "Die Hard", number_in_stock: 5, daily_rental_rate: 2, genre_id: action.id },
+    { title: "Terminator", number_in_stock: 10, daily_rental_rate: 2, genre_id: action.id },
+    { title: "The Avengers", number_in_stock: 15, daily_rental_rate: 2, genre_id: action.id }
+  ]
+
+  romance = Genre.find_by(name: 'Romance')
+  Movie.create! [
+    { title: "The Notebook", number_in_stock: 5, daily_rental_rate: 2, genre_id: romance.id },
+    { title: "When Harry Met Sally", number_in_stock: 10, daily_rental_rate: 2, genre_id: romance.id },
+    { title: "Pretty Woman", number_in_stock: 15, daily_rental_rate: 2, genre_id: romance.id }
+  ]
+
+  thriller = Genre.find_by(name: 'Thriller')
+  Movie.create! [
+    { title: "The Sixth Sense", number_in_stock: 5, daily_rental_rate: 2, genre_id: thriller.id },
+    { title: "Gone Girl", number_in_stock: 10, daily_rental_rate: 2, genre_id: thriller.id },
+    { title: "The Others", number_in_stock: 15, daily_rental_rate: 2, genre_id: thriller.id }
+  ]
+
+  p "#{DateTime.now} Created Movies, (#{Movie.count} items)."
+end

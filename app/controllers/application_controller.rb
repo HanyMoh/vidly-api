a11a93961edbd45815ada39812d88a8de73e4652
HanyMoh <hanymoh@msn.com>
2019-09-28 18:47:23 +0200

    Fixing invalid authenticator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, only: proc { |c| c.request.format.json? }
 end
